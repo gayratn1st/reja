760101c1b5ba81698fb28c224fe61046cd774936
gayratn1st <gayratn1st@gmail.com>
2025-09-18 17:24:15 +0900

    BRR: build express web server

---
+++ server.js
@@ -0,0 +1,55 @@
+console.log('web serverlar boshlash');
+
+const express = require('express');
+const app = express();
+const http = require('http')
+
+//1 kirish
+app.use(express.static('public'));
+
+app.use(express.json());
+
+app.use(express.urlencoded({extended: true}));
+console.log('GAy')
+
+
+
+//  2 session
+
+// 3  Views
+
+app.set("views" , 'views');
+app.set("view engine" , 'ejs');
+
+console.log('GAy1')
+
+//4 routing  req
+app.get("/" , function ( req ,  res) {
+    res.end('hi world')
+});
+app.get("/gift" , function ( req ,  res) {
+    res.end('mana sizga sovga')
+});
+const server = http.createServer(app);
+let PORT = 3000;
+server.listen(PORT , function(){
+    console.log(`THe server is running succesfully ${PORT} `);
+})
+
+console.log('GAy')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
