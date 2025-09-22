888d1e210e525644e2140f5fa484f0c67502dff0
gayratn1st <gayratn1st@gmail.com>
2025-09-23 05:27:47 +0900

    feat:train callback and async functions

--- server.js
+++ server.js
@@ -1,33 +1,39 @@
 console.log('web serverlar boshlash');
-
 const express = require('express');
+// const res = require("express/lib/response")
 const app = express();
 const http = require('http')
+const fs = require("fs");
 
+let user;
+fs.readFile("database/user.json" , "utf8", (err,data) => {
+    if(err) {
+        console.log('ERROR:' , err);
+    } 
+    else {
+        user = JSON.parse(data)
+    }
+});
 //1 kirish
 app.use(express.static('public'));
-
 app.use(express.json());
-
 app.use(express.urlencoded({extended: true}));
-console.log('GAy')
-
-
 
 //  2 session
-
 // 3  Views
 
 app.set("views" , 'views');
 app.set("view engine" , 'ejs');
-
-console.log('GAy1')
 
 // 4 Routing code
 app.post("/create-item", (req, res) => {
     console.log(req.body);
     res.json({ test: "success" });
 });
+
+app.get('/author', (req,res)=> {
+    res.render("author",{user: user} );
+})
 
 app.get("/" , function ( req ,  res) {
     res.render('harid');
